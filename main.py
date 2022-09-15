bc8b8ad2832b843124b312c36c1b2709f6751328
andrewjanedy <andy.janedy@gmail.com>
2022-09-15 15:16:25 -0400

    Added comments

--- main.py
+++ main.py
@@ -6,13 +6,19 @@ current_year = 2022
 my_name = "Andrew Janedy"
 my_age = current_year - my_birth_year
 
+# Output concatenating assigned variables and string literals
 print(f"\n{my_name} will turn {my_age} this year\n")
 
+# Asking user for input
 user_birth_year = input("What year were you born?: ")
+
+# Declaring and assigning a variable by converting user input to an integer
 user_age = current_year - int(user_birth_year)
 
+# Output concatenating assigned variables and string literals
 print(f"\nYou will turn {user_age} this year")
 
+# if/elif statements to output appropriate message depending on user input
 if (user_age == my_age):
     print("We are the same age!")
 elif (user_age > my_age):
